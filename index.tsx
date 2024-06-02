3db1b1bcccd65f28d8d32e343e31aebbc6894207
Sean Scally <scally@users.noreply.github.com>
2024-06-02 15:29:07 -0700

    Expose bug in kitchen-sink sample
    bug: Global Nums doesn't render until the next rendercycle, it should immediately render on button click

--- index.tsx
+++ index.tsx
@@ -4,14 +4,13 @@ const server = Bun.serve(
   live({
     shared: {
       count: 0,
+      nums: Array<number>()
     },
     local: {
       count: 0,
       nums: Array<number>()
     },
     mount: ({addTimer, local}) => {
-      local.count = 8
-      local.nums = [17]
       addTimer(1000, {
         type: 'tick'
       })
@@ -24,11 +23,21 @@ const server = Bun.serve(
         <div>
           <CounterPart label='Local Count' count={local.count} />
           <CounterPart label='Global Count' count={shared.count} />
-          {
-            local.nums?.map(n => <div key={n}>{n}</div>)
-          }
+          <div style={{display: 'flex', flexDirection: 'row'}}>
+            Local Nums: 
+            {
+              local.nums?.map(n => <div key={n}>{n}</div>)
+            }
+          </div>
+          <div style={{display: 'flex', flexDirection: 'row'}}>
+            Global Nums: 
+            {
+              shared.nums?.map(n => <div key={n}>{n}</div>)
+            }
+          </div>
           <button type='button' data-stl-action='inc100'>Local Inc 100</button>
           <button type='button' data-stl-action='global-inc'>Global Inc 1</button>
+          <button type='button' data-stl-action='global-inc-nums'>Global Inc Nums</button>
         </div>
       )
     },
@@ -45,6 +54,9 @@ const server = Bun.serve(
           break
         case 'global-inc':
           shared.count += 1
+          break
+        case 'global-inc-nums':
+          shared.nums.push(shared.nums.length)
           break
         default:
           break
